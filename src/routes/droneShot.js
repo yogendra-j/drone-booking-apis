553a3c6dea229f7577f7bb3a4239b106130d1f42
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2023-03-27 00:58:14 +0530

    aaded droneshot and dronesite routes

---
+++ src/routes/droneShot.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import {
+  createDroneShot,
+  getAllDroneShots,
+  getDroneShotById,
+  updateDroneShotById,
+  deleteDroneShotById,
+} from "../services/droneShotService.js";
+
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newDroneShot = await createDroneShot(req.body);
+    res.status(201).json(newDroneShot);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allDroneShots = await getAllDroneShots();
+    res.status(200).json(allDroneShots);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const droneShot = await getDroneShotById(req.params.id);
+    res.status(200).json(droneShot);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedDroneShot = await updateDroneShotById(req.params.id, req.body);
+    res.status(200).json(updatedDroneShot);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedDroneShot = await deleteDroneShotById(req.params.id);
+    res.status(200).json(deletedDroneShot);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
---
+++ src/routes/droneSite.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import {
+  createDroneSite,
+  getAllDroneSites,
+  getDroneSiteById,
+  updateDroneSiteById,
+  deleteDroneSiteById,
+} from "../services/droneSiteService.js";
+
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newDroneSite = await createDroneSite(req.body);
+    res.status(201).json(newDroneSite);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allDroneSites = await getAllDroneSites();
+    res.status(200).json(allDroneSites);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const droneSite = await getDroneSiteById(req.params.id);
+    res.status(200).json(droneSite);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedDroneSite = await updateDroneSiteById(req.params.id, req.body);
+    res.status(200).json(updatedDroneSite);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedDroneSite = await deleteDroneSiteById(req.params.id);
+    res.status(200).json(deletedDroneSite);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
