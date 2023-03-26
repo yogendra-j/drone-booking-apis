1494b0b38dda76f9f91b941e5568f8a16e296c36
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2023-03-27 00:10:27 +0530

    added routes in server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,20 @@ import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import errorMiddleware from './middlewares/errorMiddleware.js';
+import customerRoutes from './routes/customer.js';
+import bookingRoutes from './routes/booking.js';
+import droneShotRoutes from './routes/droneShot.js';
+import droneSiteRoutes from './routes/droneSite.js';
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(json());
+
+app.use('/customers', customerRoutes);
+app.use('/bookings', bookingRoutes);
+app.use('/drone-shots', droneShotRoutes);
+app.use('/drone-sites', droneSiteRoutes);
 
 app.use(errorMiddleware);
 
