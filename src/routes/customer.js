8855d3677eae6302a34cf31f2e36cc0b5aaf501d
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2023-03-27 21:37:37 +0530

    handling async errors

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -9,45 +9,65 @@ import {
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const customers = await getAllCustomers();
-  res.json(customers);
-});
-
-router.get("/:id", async (req, res) => {
-  const customerId = parseInt(req.params.id);
-  const customer = await getCustomerById(customerId);
-  if (customer) {
-    res.json(customer);
-  } else {
-    res.status(404).send("Customer not found");
+router.get("/", async (req, res, next) => {
+  try {
+    const customers = await getAllCustomers();
+    res.json(customers);
+  } catch (err) {
+    next(err);
   }
 });
 
-router.post("/", async (req, res) => {
-  const newCustomer = req.body;
-  const createdCustomer = await createCustomer(newCustomer);
-  res.json(createdCustomer);
-});
-
-router.put("/:id", async (req, res) => {
-  const customerId = parseInt(req.params.id);
-  const updatedCustomer = req.body;
-  const result = await updateCustomer(customerId, updatedCustomer);
-  if (result === 1) {
-    res.json(updatedCustomer);
-  } else {
-    res.status(404).send("Customer not found");
+router.get("/:id", async (req, res, next) => {
+  try {
+    const customerId = parseInt(req.params.id);
+    const customer = await getCustomerById(customerId);
+    if (customer) {
+      res.json(customer);
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  const customerId = parseInt(req.params.id);
-  const result = await deleteCustomer(customerId);
-  if (result === 1) {
-    res.sendStatus(200);
-  } else {
-    res.status(404).send("Customer not found");
+router.post("/", async (req, res, next) => {
+  try {
+    const newCustomer = req.body;
+    const createdCustomer = await createCustomer(newCustomer);
+    res.json(createdCustomer);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const customerId = parseInt(req.params.id);
+    const updatedCustomer = req.body;
+    const result = await updateCustomer(customerId, updatedCustomer);
+    if (result === 1) {
+      res.json(updatedCustomer);
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const customerId = parseInt(req.params.id);
+    const result = await deleteCustomer(customerId);
+    if (result === 1) {
+      res.sendStatus(200);
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
