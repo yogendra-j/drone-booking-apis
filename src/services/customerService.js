4c962046905fc482263b2634dd3025e326463212
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2023-03-27 21:21:23 +0530

    fixing bug in argument destructuring

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,51 +1,76 @@
-import prisma  from "../db/prisma.js";
+import prisma from "../db/prisma.js";
 
 async function getAllCustomers() {
-  const customers = await prisma.customer.findMany();
-  return customers;
+  try {
+    const customers = await prisma.customer.findMany();
+    return customers;
+  } catch (error) {
+    console.error("Error retrieving customers:", error);
+    throw error;
+  }
 }
 
 async function getCustomerById(id) {
-  const customer = await prisma.customer.findUnique({
-    where: {
-      id: parseInt(id)
-    }
-  });
-  return customer;
+  try {
+    const customer = await prisma.customer.findUnique({
+      where: {
+        id: parseInt(id)
+      }
+    });
+    return customer;
+  } catch (error) {
+    console.error(`Error retrieving customer with ID ${id}:`, error);
+    throw error;
+  }
 }
 
-async function createCustomer(name, email, phoneNumber) {
-  const newCustomer = await prisma.customer.create({
-    data: {
-      name: name,
-      email: email,
-      phoneNumber: phoneNumber
-    }
-  });
-  return newCustomer;
+async function createCustomer({name, email, phoneNumber}) {
+  try {
+    const newCustomer = await prisma.customer.create({
+      data: {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber
+      }
+    });
+    return newCustomer;
+  } catch (error) {
+    console.error("Error creating new customer:", error);
+    throw error;
+  }
 }
 
-async function updateCustomer(id, name, email, phoneNumber) {
-  const updatedCustomer = await prisma.customer.update({
-    where: {
-      id: parseInt(id)
-    },
-    data: {
-      name: name,
-      email: email,
-      phoneNumber: phoneNumber
-    }
-  });
-  return updatedCustomer;
+async function updateCustomer({id, name, email, phoneNumber}) {
+  try {
+    const updatedCustomer = await prisma.customer.update({
+      where: {
+        id: parseInt(id)
+      },
+      data: {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber
+      }
+    });
+    return updatedCustomer;
+  } catch (error) {
+    console.error(`Error updating customer with ID ${id}:`, error);
+    throw error;
+  }
 }
 
 async function deleteCustomer(id) {
-  const deletedCustomer = await prisma.customer.delete({
-    where: {
-      id: parseInt(id)
-    }
-  });
-  return deletedCustomer;
+  try {
+    const deletedCustomer = await prisma.customer.delete({
+      where: {
+        id: parseInt(id)
+      }
+    });
+    return deletedCustomer;
+  } catch (error) {
+    console.error(`Error deleting customer with ID ${id}:`, error);
+    throw error;
+  }
 }
 
 export {
